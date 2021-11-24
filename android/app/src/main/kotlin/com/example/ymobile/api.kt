75b3319c987ab01cef62ba84957a189fe7b34651
unknown <koi_town@outlook.com>
2021-11-24 19:49:31 +0800

    Finish api

--- android/app/src/main/kotlin/com/example/ymobile/api.kt
+++ android/app/src/main/kotlin/com/example/ymobile/api.kt
@@ -1,30 +1,37 @@
 package com.example.ymobile
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
+import java.net.DatagramSocket
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
+import java.net.DatagramPacket
 
 
-class api {
-    api(socket)
-    queryIp() {
-        sendRequets(ApiId.C2S_QUERY_IP)
+class api(socket:DatagramSocket) {
+    var socket = socket
+    fun queryIp() {
+         sendRequest(ApiId.C2S_QUERY_IP,null)
     }
 
-    sendRequest(msgId,bodyMsg?) {
-        var bodyBytes:Bytes
-        if bodyMsg != nil {
-            bodyBytes  = json.encodeToString(bodyMsg)
+    fun sendRequest(msgId:ApiId,bodyMsg:Serializable?) {
+        var bodyBytes:String = ""
+        if (bodyMsg !=null) {
+            bodyBytes  = Json.encodeToString(bodyMsg)
         }
-        var req = ApiReq(msgId,bodyBytes)
-        var msgBytes  = json.marshal(req)
-        socket.write(msgBytes)
+        var req = ApiReq(msgId,bodyBytes?.toByteArray())
+        var msgBytes  = Json.encodeToString(req)
+        var bytes = ByteArray(msgBytes.length+1)
+        bytes = msgBytes.toByteArray().copyInto(bytes,1,0,msgBytes.length)
+        socket.send(DatagramPacket(bytes,msgBytes.length+1))
     }
 
-    fun Res decodeRes(resMsg) {
-        return  json.decodeFromString<Res>(resMsg)
+    fun <T>decodeRes(resMsg: DeserializationStrategy<Res>, resMsgBytes:ByteArray) :Res {
+        return Json.decodeFromString<Res>(resMsg,resMsgBytes.toString())
     }
 
-    fun decodeResBody<T>(resBodyBytes):T{
-        return json.decodeFromString<T>(resBodyBytes)
+    fun <T>decodeResBody(resBody:DeserializationStrategy<T>,resBodyBytes:ByteArray):T{
+        return Json.decodeFromString<T>(resBody,resBodyBytes.toString())
     }
 }
 
@@ -38,11 +45,12 @@ class Res{
 }
 
 @Serializable
-class ApiReq{
-    ApiReq(msgId,msgBodyBytes)
+class ApiReq(msgId:ApiId,msgBodyBytes:ByteArray?){
+    var msgId =msgId
+    var msgBodyBytes=msgBodyBytes
 }
 
-enum class ApiId(id:Int){
-    C2S_QUERY_IP = 1
+enum class ApiId{
+    C2S_QUERY_IP,
 
 }
