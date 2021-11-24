48fcd41a37a6d1a139e0dabc9cafb8958ce574be
tzz <koi_town@outlook.com>
2021-11-24 13:04:14 +0800

    Add json

--- android/app/src/main/kotlin/com/example/ymobile/api.kt
+++ android/app/src/main/kotlin/com/example/ymobile/api.kt
@@ -1,15 +1,48 @@
 package com.example.ymobile
 
+import kotlinx.serialization.Serializable
+
+
 class api {
     api(socket)
-    queryIp(){
-
-    }
-    doRequest(){
-
+    queryIp() {
+        sendRequets(ApiId.C2S_QUERY_IP)
     }
 
-    decodeRes(){
-
+    sendRequest(msgId,bodyMsg?) {
+        var bodyBytes:Bytes
+        if bodyMsg != nil {
+            bodyBytes  = json.encodeToString(bodyMsg)
+        }
+        var req = ApiReq(msgId,bodyBytes)
+        var msgBytes  = json.marshal(req)
+        socket.write(msgBytes)
     }
+
+    fun Res decodeRes(resMsg) {
+        return  json.decodeFromString<Res>(resMsg)
+    }
+
+    fun decodeResBody<T>(resBodyBytes):T{
+        return json.decodeFromString<T>(resBodyBytes)
+    }
+}
+
+@Serializable
+class reqQueryIp{
+
+}
+@Serializable
+class Res{
+
+}
+
+@Serializable
+class ApiReq{
+    ApiReq(msgId,msgBodyBytes)
+}
+
+enum class ApiId(id:Int){
+    C2S_QUERY_IP = 1
+
 }
