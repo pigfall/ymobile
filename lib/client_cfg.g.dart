d332300d5091474073a2125b3bb0956561c0fe83
unknown <koi_town@outlook.com>
2021-11-28 18:10:27 +0800

    update

---
+++ lib/client_cfg.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'client_cfg.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ClientCfg _$ClientCfgFromJson(Map<String, dynamic> json) => ClientCfg(
+      id: json['id'] as String,
+      serverHost: json['serverHost'] as String,
+      serverPort: json['serverPort'] as String,
+    );
+
+Map<String, dynamic> _$ClientCfgToJson(ClientCfg instance) => <String, dynamic>{
+      'id': instance.id,
+      'serverHost': instance.serverHost,
+      'serverPort': instance.serverPort,
+    };
